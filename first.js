5ea077f9533df0e51dc0cbd32a12a6d0b9edc7a5
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-10-17 10:45:00 +0530

    sportify landing page

---
+++ first.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed('#typed', {
+    
+    strings: ["FREE DELIVERY RETURN AND EXCHANGE.", "SIGN UP AND GET 15% OFF", "EXTRA 5% OFF ON PREPAID ORDERS"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 100,
+    startDelay: 100,
+    loop: true  ,
+  });
